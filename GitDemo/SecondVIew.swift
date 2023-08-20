ea97ad5475d16ebf8643f12d1e1bb160a01616cb
Sonic Liu <liuhanyang0730@gmail.com>
2023-08-20 11:18:24 +0800

    Created Second View

---
+++ GitDemo/SecondVIew.swift
@@ -0,0 +1,22 @@
+//
+//  SecondVIew.swift
+//  GitDemo
+//
+//  Created by Sonic Liu on 20/8/2023.
+//
+
+import SwiftUI
+
+struct SecondVIew: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct SecondVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondVIew()
+    }
+}
+
+
---
+++ GitDemo/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  GitDemo
+//
+//  Created by Sonic Liu on 20/8/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
