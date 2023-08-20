da2c4f56f2cf939a1dd4a90f10fb21ade6643156
Sonic Liu <liuhanyang0730@gmail.com>
2023-08-20 11:32:04 +0800

    BackGround Colour changed from white to red

--- GitDemo/ContentView.swift
+++ GitDemo/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("This is a test")
+        ZStack {
+            Color.red.ignoresSafeArea(.all)
+            VStack(alignment: .trailing) {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("This is a test")
+            }
+            
+            .padding()
         }
-        .padding()
     }
 }
 
