2a790b7f40c3358927526bb15f35d5ffec0932b7
Sonic Liu <liuhanyang0730@gmail.com>
2023-08-20 11:10:56 +0800

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Sonic Liu on 20/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
